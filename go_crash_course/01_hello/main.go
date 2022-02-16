9351dabda490b3600edb52408281294a6a69cdf9
dave <dave.m.mcginn@gmail.com>
2022-02-16 08:48:30 +0000

    adding return example

--- go_crash_course/01_hello/main.go
+++ go_crash_course/01_hello/main.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+	greetings := hello("dave")
+	fmt.Println(greetings)
+}
+
+func hello(s string) string {
+	greet := fmt.Sprintf("Hello ")
+	return greet + s
 }
